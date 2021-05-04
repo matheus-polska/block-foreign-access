fbdb5ec3b2fe852df25211033bdf68824a6ff365
matheus-polska <46266929+matheus-polska@users.noreply.github.com>
2021-05-04 15:56:12 -0300

    test

--- middleware/auth-local-ip.js
+++ middleware/auth-local-ip.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-export default async function ({ req }) {
+export default async function ({ req, error }) {
   const headers = req && req.headers ? Object.assign({}, req.headers) : {}
   const ip = headers['x-forwarded-for']
   const { data } = await axios.get(
     `https://api.ipgeolocation.io/ipgeo?apiKey=187f062c0c074f89b2a706343f6d69a8&ip=${ip}`
   )
   if (data.country_name !== 'Brazil') {
-    return error({ message: 'Not allowed', statusCode: '404' })
+    return error({ message: 'Not allowed', statusCode: '403' })
   }
 }
