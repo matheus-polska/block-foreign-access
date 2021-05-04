a8f490855fbd35c700dc5a8b3255a5bb1d80d52d
matheus-polska <46266929+matheus-polska@users.noreply.github.com>
2021-05-04 15:06:09 -0300

    test

--- middleware/auth-local-ip.js
+++ middleware/auth-local-ip.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export default async function ({ error }) {
+export default async function ({ req }) {
   const ip = req.connection.remoteAddress
   console.log('<<<MEU IP:', ip)
   const { data } = await axios.get(
